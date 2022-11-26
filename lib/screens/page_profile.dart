e0bf4a9fd5e478a9d1169e647ede52982758f091
ZARIFMAC <uzbzarif@gmail.com>
2022-11-26 15:53:19 +0500

    user

--- lib/screens/page_profile.dart
+++ lib/screens/page_profile.dart
@@ -1,17 +1,25 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:randomusers_api/randomusers.dart';
 
 class PageProfile extends StatelessWidget {
-  const PageProfile({Key? key}) : super(key: key);
-
+  PageProfile({Key? key}) : super(key: key);
+  Randomusers user = Randomusers('firstName', 'lastName');
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Profile'),
       ),
-      body: Container(),
+      body: Container(
+        child: Column(
+          children: [
+            Text(user.firstName),
+            Text(user.lastName),
+          ],
+        ),
+      ),
     );
   }
 }
