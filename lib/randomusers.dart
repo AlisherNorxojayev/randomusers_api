fb9e9b22fcd9e0c764cebf9b8b669e409e938b67
ZARIFMAC <uzbzarif@gmail.com>
2022-11-29 16:23:32 +0500

    done

--- lib/randomusers.dart
+++ lib/randomusers.dart
@@ -1,5 +1,10 @@
 class Randomusers {
   final String firstName;
   final String lastName;
+
   Randomusers(this.firstName, this.lastName);
+
+  Randomusers.fromJson(Map data)
+      : firstName = data['first'],
+        lastName = data['last'];
 }
